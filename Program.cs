cfa0e065a25930f328177049ca61f368fe5358ac
paetalung <paetalung@gmail.com>
2019-06-10 07:29:10 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace demo08.doWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Summon a monster!"); // Create monster
+                break;  // Just stop it.
+            } while (true);
+            Console.WriteLine("End.");
+        }
+    }
+}
